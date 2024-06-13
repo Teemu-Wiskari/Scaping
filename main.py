6ef8472f1ba025192d83883a4a5434df8d925e35
Arti <145042815+Teemu-Wiskari@users.noreply.github.com>
2024-06-14 00:59:27 +0300

    Editing main.py
    simplified the code

--- main.py
+++ main.py
@@ -19,14 +19,20 @@ response = requests.get(URL, headers=gen_headers())
 main_html = response.text
 main_page = bs4.BeautifulSoup(main_html, "lxml")
 vacancies = main_page.find_all("div", class_="vacancy-serp__results", id="a11y-main-content")
-results = []
+vacancy_info = []
 
 for vacancy in vacancies:
-    link = vacancy.find("a", class_="bloko-link")['href']
-    name_company = vacancy.find("a", class_="bloko-link").text
-    city = vacancy.find("span", class_="fake-magritte-primary-").text
-    salary = vacancy.find("span", class_="fake-magritte-primary-").text
+    link = vacancy.find("a", class_="bloko-link").get('href')
+    company = vacancy.find("span", class_="company-info-text--vgvZouLtf8jwBmaD1xgp").text
+    city = vacancy.find("span", class_="fake-magritte-primary-text--Hdw8FvkOzzOcoR4xXWni").text
+    if city:
+        city = city.strip()
+    else:
+        city = "Не указан"
 
+    salary = vacancy.find("span",
+                          class_="fake-magritte-primary-text--Hdw8FvkOzzOcoR4xXWni compensation-text--kTJ0_"
+                                 "rp54B2vNeZ3CTt2 separate-line-on-xs--mtby5gO4J0ixtqzW38wh").text
     if salary:
         salary = salary.text.strip()
     else:
@@ -34,13 +40,12 @@ for vacancy in vacancies:
 
     description = vacancy.find(class_='g-user-content').text
     if any(key.lower() in description.lower() for key in keys):
-        info = {
-            'link': link,
-            'name_company': name_company,
+        vacancy_info.append({
+            'link': f"{link}",
+            'company': company,
             'city': city,
             'salary': salary
-        }
-        results.append(info)
+        })
 
 with open('results.json', 'w', encoding='utf-8') as f:
-    json.dump(results, f, ensure_ascii=False, indent=4)
+    json.dump(vacancy_info, f, ensure_ascii=False, indent=4)
